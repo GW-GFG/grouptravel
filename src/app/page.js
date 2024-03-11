56a7eb2fc4fbffaf6ea41243a4b025bdcdd40aa0
KB-Toast <kevin.brusson@gmail.com>
2024-03-11 16:14:57 +0100

    fixing CSS for dashboard page and menu

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,14 @@ import styles from "./page.module.css";
 import Dashboard from "@/components/Dashboard";
 import { useSelector } from "react-redux";
 import NotConnected from "@/components/missingInfos/NotConnected";
+import HomeNotConnected from "@/components/missingInfos/HomeNotConnected";
 
 export default function Home() {
   
   const user = useSelector((state) => state.user.value);
     if (!user.token) {
         return (
-          <NotConnected />
+          <HomeNotConnected />
         );
     } else {
         return <Dashboard />
