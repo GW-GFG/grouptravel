e606353d3e0f700a0b8c4a48a3347856848e947a
Jean-Baptiste Cubizolles <cubizollesjb@gmail.com>
2024-03-14 16:57:21 +0100

    added fields verification on CreateTrip, alert for location error on CreateTrip, AddActivity and AddAccomodation

--- src/components/CreateTrip.js
+++ src/components/CreateTrip.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { notification } from 'antd'
 import InputLabel from './InputLabel';
 import styles from './creatTrip.module.css';
-import {updateCurrentTrip, updateMyTrips} from '../reducers/user';
+import { updateCurrentTrip, updateMyTrips } from '../reducers/user';
 // import fonts to use them for menu items
 import { lexend } from '../app/fonts';
 
@@ -21,73 +21,86 @@ export default function CreateTrip() {
     const [errorMsg, setErrorMsg] = useState('')
 
     const token = user.token
-     
+
 
     const handleSubmit = () => {
-
+        if (!location || !groupName || !departureDate || !returnDate) {
+            notification.warning({
+                message: 'Attention !',
+                description: 'Merci de remplir tous les champs.',
+                placement: 'bottomRight'
+            })
+            return
+        }
         // Google map input logic
         fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAtN3JpGGPLuZkaD7j2zoSB0vE3e_B-Jn8&address=${location}`)
-        .then(response => response.json()).then(data => {
-            if (data && data.results[0]) {
-                const coordinates = data.results[0].geometry.location;
-
-                // Unbreakable form
-                const locationToSend = {
-                    name: location[0].toUpperCase()+ location.slice(1).toLowerCase(),
-                    lat: coordinates.lat,
-                    lng: coordinates.lng,
-                }
-                const nameToSend = groupName[0].toUpperCase()+ groupName.slice(1).toLowerCase()
-                // fetch for add new trip in DDB
-                fetch('http://localhost:5500/trips/new', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ name: nameToSend, location : locationToSend, departureDate, returnDate, token: token }),
-                }).then(response => response.json())
-                    .then(data => {
-                    // If data.error send error.msg to front
-                    if(data.error) {
-                        setErrorMsg(data.error)
-                        notification.warning({
-                            message: 'Attention !',
-                            description: data.error,
-                            placement: 'bottomRight'
-                        })
+            .then(response => response.json()).then(data => {
+                if (data && data.results[0]) {
+                    const coordinates = data.results[0].geometry.location;
+                    // Unbreakable form
+                    const locationToSend = {
+                        name: location[0].toUpperCase() + location.slice(1).toLowerCase(),
+                        lat: coordinates.lat,
+                        lng: coordinates.lng,
                     }
-                    else {
-                    // If no error > update reducer in redux
-                    // console.log('data.newTrip', data.newTrip)
-                    dispatch(updateMyTrips(data.newTrip))
-                    dispatch(updateCurrentTrip(data.newTrip))
-                    notification.success({
-                        message: 'Voyage créé !',
-                        description: 'Votre voyage a bien été créé !',
-                        placement: 'bottomRight'
+                    console.log(locationToSend)
+                    const nameToSend = groupName[0].toUpperCase() + groupName.slice(1).toLowerCase()
+                    // fetch for add new trip in DDB
+                    fetch('http://localhost:5500/trips/new', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({ name: nameToSend, location: locationToSend, departureDate, returnDate, token: token }),
+                    }).then(response => response.json())
+                        .then(data => {
+                            // If data.error send error.msg to front
+                            if (data.error) {
+                                setErrorMsg(data.error)
+                                notification.warning({
+                                    message: 'Attention !',
+                                    description: errorMsg,
+                                    placement: 'bottomRight'
+                                })
+                            }
+                            else {
+                                // If no error > update reducer in redux
+                                // console.log('data.newTrip', data.newTrip)
+                                dispatch(updateMyTrips(data.newTrip))
+                                dispatch(updateCurrentTrip(data.newTrip))
+                                notification.success({
+                                    message: 'Voyage créé !',
+                                    description: 'Votre voyage a bien été créé !',
+                                    placement: 'bottomRight'
+                                })
+                                // rerouting user to dashboard of new trip
+                                router.push('/')
+                            }
+                        });
+                } else {
+                    notification.warning({
+                        message: 'Localisation non trouvée',
+                        description: "L'adresse saisie n'a pas pu être trouvée. Veuillez essayer avec une autre adresse.",
+                        placement: 'bottomRight',
                     })
-                    // rerouting user to dashboard of new trip
-                    router.push('/')
-                    }
-                });
-            }
-        });
+                }
+            });
     }
 
     return (
         <div className={styles.container}>
             <div className={`${styles.logo} ${lexend.className}`}>GROUPTRAVEL</div>
             <h3 className={`${styles.title} ${lexend.className}`}>Planification de votre voyage entre amis !</h3>
-                <div className={styles.inputTextContainer}>
-                    <h4 className={styles.h4}>Défini le nom de ton groupe :</h4>
-                    <InputLabel style={{width: "60%"}} type="text" onChange={(e) => setgroupName(e.target.value)} value={groupName} label="Nom du groupe" placeholder="Entre le nom de ton Groupe !" />
-                </div>
-                <div className={styles.inputTextContainer}>
-                    <h4 className={styles.h4}>Précise la destination :</h4>
-                    <InputLabel type="text" onChange={(e) => setLocation(e.target.value)} value={location} label="Destination" placeholder="Et votre destination !" />
-                </div>
-                <h4 className={styles.inputTextContainer}>Vous partez quand ?</h4>
-            
+            <div className={styles.inputTextContainer}>
+                <h4 className={styles.h4}>Défini le nom de ton groupe :</h4>
+                <InputLabel style={{ width: "60%" }} type="text" onChange={(e) => setgroupName(e.target.value)} value={groupName} label="Nom du groupe" placeholder="Entre le nom de ton Groupe !" />
+            </div>
+            <div className={styles.inputTextContainer}>
+                <h4 className={styles.h4}>Précise la destination :</h4>
+                <InputLabel type="text" onChange={(e) => setLocation(e.target.value)} value={location} label="Destination" placeholder="Et votre destination !" />
+            </div>
+            <h4 className={styles.inputTextContainer}>Vous partez quand ?</h4>
+
             <div className={styles.dateContainers}>
-                <div className={styles.dateRow}>  
+                <div className={styles.dateRow}>
                     <div className={styles.inputDateContainer}>
                         <h5 className={styles.textDate}>Date de départ :</h5>
                         <input className={styles.inputDate}
@@ -106,14 +119,14 @@ export default function CreateTrip() {
                             onChange={(e) => setReturnDate(e.target.value)}
                         />
                     </div>
-                    
+
                 </div>
                 {/* only if error display error */}
                 {/* {errorMsg != '' && <h2 className={styles.error}>{errorMsg}</h2>} */}
                 <button className={styles.button} onClick={() => handleSubmit()}>Go!</button>
             </div>
-        </div>  
-                
-            
+        </div>
+
+
     )
 }
