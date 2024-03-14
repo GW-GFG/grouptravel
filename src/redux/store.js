c475909777e84f80b79f19628468d7f6177df474
KB-Toast <kevin.brusson@gmail.com>
2024-03-14 17:41:28 +0100

    added persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,9 @@
+/* ANCIENNE VERSION FONCTIONNELLE 
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-
-/* ---------------------------------------------------------------- */
-/*                      Reducers' configuration                     */
-/* ---------------------------------------------------------------- */
 
 import user from "../reducers/user";
 
 const reducers = combineReducers({ user });
-
-/* ---------------------------------------------------------------- */
-/*                        Store configuration                       */
-/* ---------------------------------------------------------------- */
 
 export const makeStore = () => {
 	const store = configureStore({
@@ -18,4 +11,44 @@ export const makeStore = () => {
 		middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 	});
 	return { store };
+};
+// END ANCIENNE VERSION FONCTIONNELLE */
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist"; // Persistor
+import storage from "./webStorage";
+
+
+/* ---------------------------------------------------------------- */
+/*                      Reducers' configuration                     */
+/* ---------------------------------------------------------------- */
+
+import user from "@/reducers/user";
+
+const reducers = combineReducers({ user });
+
+/* ---------------------------------------------------------------- */
+/*                      Persistor configuration                     */
+/* ---------------------------------------------------------------- */
+
+const persistConfig = {
+	key: "jyogames",
+	storage,
+	blacklist: [],
+	whitelist: ["user"],
+};
+
+const persistedReducers = persistReducer(persistConfig, reducers);
+
+/* ---------------------------------------------------------------- */
+/*                        Store configuration                       */
+/* ---------------------------------------------------------------- */
+
+export const makeStore = () => {
+	const store = configureStore({
+		reducer: persistedReducers,
+		middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+	});
+	const persistor = persistStore(store);
+	return { store, persistor };
 };
