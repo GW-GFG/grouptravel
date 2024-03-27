e87fa79a9c46dfb1b66864c13413d0b99fa4ed9c
Gwendoline Gaches <gwendoline.gaches@outlook.fr>
2024-03-27 16:45:47 +0100

    envoie config next

--- next.config.mjs
+++ next.config.mjs
@@ -21,8 +21,29 @@ const nextConfig = {
               pathname: '/**',
             },
           ],
-        
         },
+        async headers() {
+          return [
+            {
+              source: "https://grouptravel-b-gwgfg.vercel.app",
+              headers: [
+                { key: "Access-Control-Allow-Origin", value: "*" },
+                { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+                { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type, Authorization" },
+                { key: "Access-Control-Allow-Credentials", value: "true" }
+              ]
+            },
+            {
+              source: "/",
+              headers: [
+                { key: "Access-Control-Allow-Origin", value: "https://grouptravelgw-gfg.vercel.app" },
+                { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+                { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type, Authorization" },
+                { key: "Access-Control-Allow-Credentials", value: "true" }
+              ]
+            }
+          ];
+        }
 };
 
 export default nextConfig;
